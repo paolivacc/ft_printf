ac9c1f39a431d15f6fedf0d44474a20e344b874b
Susana Paoli Vaccaro Nunez <svaccaro@c3r8s2.42malaga.com>
2023-10-29 22:55:36 +0100

    Set up the ft_printf.c function structure

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,30 @@
 /*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:45:09 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/10/25 14:48:53 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/10/29 22:49:26 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include ""
+#include "ft_pritf.h"
 
-int	ft_printf(char const *, ...)
+int	ft_printf(char const *s, ...)
 {
+	va_list	ptr;
+	size_t	bytcnt;
+	size_t	i;
 
-	return();
+	i = 0;
+	bytcnt = 0;
+	va_start(ptr, s);
+	while (ptr[i])
+	{
+		if (ptr[i] == %)
+		{
+			i++;
+			bytcnt += ft_format(ptr[i], va_arg(ptr, char));
+		}
+		else
+			bytcnt += ft_putchar(ptr[i]);
+	}
+	return(bytcnt);
 }
