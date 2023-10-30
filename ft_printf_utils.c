3018e70e466c4b6a2831e3f4169d13f972e87f4d
Susana Paoli Vaccaro Nunez <svaccaro@c3r10s2.42malaga.com>
2023-10-30 22:29:03 +0100

    Create function ft_format

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf_utils.c                                     :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 21:53:10 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/10/29 22:49:56 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/10/30 19:34:00 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_putchar(char c)
 {
-	return(write(STDOUT_FILENO, &c, sizeof(c)));
+	return (write(STDOUT_FILENO, &c, sizeof(c)));
 }
 
 size_t	ft_putstr(char *s)
@@ -31,10 +31,28 @@ size_t	ft_putstr(char *s)
 	return (bytcnt);
 }
 
-size_t	ft_format(char c, char param)
+size_t	ft_format(char c, va_list param)
 {
-	
+	size_t	bytcnt;
+
+	bytcnt = 0;
+	if (c == 'c')
+		bytcnt += ft_putchar(va_arg(param, char));
+	else if (c == 's')
+		bytcnt += ft_putstr(va_arg(param, char));
+	else if (c == 'p')
+		;
+	else if (c == 'd')
+		;
+	else if (c == 'i')
+		;
+	else if (c == 'u')
+		;
+	else if (c == 'x')
+		;
+	else if (c == 'X')
+		;
+	else if (c == '%')
+		bytcnt += ft_putchar('%');
+	return (bytcnt);
 }
-
-
-
