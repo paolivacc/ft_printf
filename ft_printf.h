dea0b5feceb849c70ec25c8c3700346c826105d8
Susana Paoli Vaccaro Nunez <svaccaro@c3r5s2.42malaga.com>
2023-11-10 15:07:13 +0100

    Modify ft_putchar funtion declaration

--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,17 @@
 /*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 21:53:57 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/11/09 20:30:10 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/11/10 15:06:00 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <unistd.h>
 # include <stdarg.h>
-# include <stdio.h>
-# include <limits.h>
+# include "libft.h"
 
-int	ft_putchar(char c);
+int	ft_putchar(int c);
 int	ft_putstr(char *s);
 int	ft_format(char c, va_list param);
 
